fd704220a09cf8a388656b5b8a2f6199668f93c6
Debian Live user <amnesia@localhost.localdomain>
2015-09-04 07:23:22 +0000

    rm numpy, switch to curl, update cfg, update sig

--- tailsjoin.sh
+++ tailsjoin.sh
@@ -25,15 +25,15 @@ sudo apt-get update
 # Install dependencies available with apt-get
 clear
 echo -e "\n\nENTER PASSWORD AT PROMPT TO INSTALL THE FOLLOWING DEPENDENCIES:\n"
-echo -e "gcc, libc6-dev, make, python-dev, python-pip\n"
-sudo apt-get install -y gcc libc6-dev make python-dev python-pip
+echo -e "gcc, libc6-dev, make\n"
+sudo apt-get install -y gcc libc6-dev make
 # Fetch libsodium and sig, import key, and verify.
 clear
 echo -e "\n\nPRESS ENTER TO GET SIGNING KEYS AND VERIFY LIBSODIUM SOURCE FROM:\n"
 read -p "http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz"
 gpg --recv-keys 54A2B8892CC3D6A597B92B6C210627AABA709FE1
 echo "54A2B8892CC3D6A597B92B6C210627AABA709FE1:6" | gpg --import-ownertrust -
-wget http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz.sig
+curl -x socks5://127.0.0.1:9050 -# -L -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz.sig
 gpg --verify libsodium-1.0.3.tar.gz.sig libsodium-1.0.3.tar.gz
 echo -e "\n\nPLEASE REVIEW SIGNATURE.\n"
 read -p "GOOD SIG? (y/n) " x
@@ -42,45 +42,35 @@ while [[ "$x" = "n" || "$x" = "N" ]]; do
   echo -e "\n\n"
   read -p "PRESS ENTER TO DELETE FILES AND GET AGAIN."
   srm -drv libsodium*
-  wget http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz.sig
+  curl -x socks5://127.0.0.1:9050 -# -L -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.3.tar.gz.sig
   gpg --verify libsodium-1.0.3.tar.gz.sig libsodium-1.0.3.tar.gz
   echo -e "\n\nPLEASE REVIEW THE SIG TO MAKE SURE IT IS GOOD.\n"
   read -p "GOOD SIG? (y/n) " x
 done
 # Build libsodium, install, and delete tar files
 tar xf libsodium-1.0.3.tar.gz
-srm -drv libsodium-1.0.3.tar.gz*
+rm -rf libsodium-1.0.3.tar.gz*
 ( cd libsodium-1.0.3/ && ./configure )
 ( cd libsodium-1.0.3/ && make )
 clear
 echo -e "\n\nLIBSODIUM SUCCESSFULLY BULIT. ENTER PASSWORD TO INSTALL.\n"
 ( cd libsodium-1.0.3/ && sudo make install )
 rm -rf libsodium-1.0.3/
-# Use pip to upgrade numpy
-clear
-echo -e "\n\nENTER PASSWORD AT PROMPT TO UPGRADE NUMPY TO VERSION 1.9.2\n"
-sudo torify pip install numpy --upgrade
 # Clone into joinmarket
-clear
-# This code for allowing amnesia access to the python modules lifted from the Axis-Mundi README.TAILS (https://github.com/six-pack/Axis-Mundi/blob/master/README.TAILS)
-echo -e "\n\nENTER PASSWORD TO ALLOW USER amnesia TO USE PYTHON MODULES WITH COMMAND:\n"
-echo -e "sudo chmod -R o+r,o+X /usr/local/lib/python2.7/dist-packages\n"
-sudo chmod -R o+r,o+X /usr/local/lib/python2.7/dist-packages
 clear
 echo -e "\n\nPRESS ENTER TO CLONE INTO JOINMARKET VIA:\n"
 read -p "https://github.com/chris-belcher/joinmarket"
 git clone https://github.com/chris-belcher/joinmarket ../joinmarket
-# Set config for tor and blockr. Tails users should consider using external media with a stored blockchain for added privacy and the tailsjoincore.sh script.
-echo -e "[BLOCKCHAIN]\nblockchain_source = blockr\n#options: blockr, json-rpc, regtest\n#before using json-rpc read https://github.com/chris-belcher/joinmarket/wiki/Running-JoinMarket-with-Bitcoin-Core-full-node\nnetwork = mainnet\nbitcoin_cli_cmd = bitcoin-cli\n\n[MESSAGING]\n#for clearnet\n#host = irc.cyberguerrilla.info\nchannel = joinmarket-pit\nusessl = true\n#for tor\nsocks5 = false\nsocks5_host = 127.0.0.1\nsocks5_port = 9050\n#host = 6dvj6v5imhny3anf.onion\nhost = a2jutl5hpza43yog.onion\n#socks5 = true\nport = 6697\n" > ../joinmarket/joinmarket.cfg
+# Set config for tor and blockr. Tails users should consider using external media with a stored blockchain for added privacy and the tailsjoin-fullnode.sh script.
+echo -e "[BLOCKCHAIN]\nblockchain_source = blockr\n#options: blockr, bitcoin-rpc, json-rpc, regtest\n#for instructions on bitcoin-rpc read https://github.com/chris-belcher/joinmarket/wiki/Running-JoinMarket-with-Bitcoin-Core-full-node \nnetwork = mainnet\nrpc_host = localhost\nrpc_port = 8332\nrpc_user = bitcoin\nrpc_password = password\n\n[MESSAGING]\n#host = irc.cyberguerrilla.org\nchannel = joinmarket-pit\nport = 6697\nusessl = true\nsocks5 = true\nsocks5_host = 127.0.0.1\nsocks5_port = 9050\n#for tor\nhost = a2jutl5hpza43yog.onion" > ../joinmarket/joinmarket.cfg
 clear
 echo -e "\n\nJOINMARKET INSTALLED, AND CONFIG SET TO USE TOR!"
 echo -e "\n\nYOU CAN FIND THE FOLDER HERE:"
 echo $PWD | sed 's|tailsjoin|joinmarket|'
-echo -e "\nHERE IS A DETAILED BEGINNERS GUIDE:"
+echo -e "\nDETAILED BEGINNERS GUIDE:"
 echo "https://github.com/tailsjoin/tailsjoin/wiki/Detailed-Minimal-Setup-Guide"
-echo -e "\nHERE IS A HIDDEN SERVICE ORDERBOOK WATCHER:"
+echo -e "\nHIDDEN SERVICE ORDERBOOK WATCHER:"
 echo "http://ruc47yiosooolrzw.onion:62601/"
-echo -e "\nHERE IS THE JOINMARKET OFFICIAL GITHUB:"
+echo -e "\nJOINMARKET OFFICIAL GITHUB:"
 echo -e "https://github.com/chris-belcher/joinmarket\n\n"
 read -p "PRESS ENTER TO EXIT. "
-exit 0
