58c210a299789bacafe96a30a30b6c49c31b1b26
tailsjoin <tailsjoin@users.noreply.github.com>
2015-10-20 06:15:34 +0000

    rm XT (too much maint.), made more readable, sig

--- tailsjoin-fullnode.sh
+++ tailsjoin-fullnode.sh
@@ -1,92 +1,111 @@
 #!/bin/bash
 set -e
 clear
-echo -e "\n\nYOU ARE CURRENTLY IN THE FOLDER $PWD"
-read -p "IS THIS WHERE YOU WANT TO DOWNLOAD BITCOIN? (y/n) " x
-if [[ "$x" = "n" || "$x" = "N" ]]; then
-  clear
-  echo -e "\n\nPLEASE MOVE THE SCRIPT tailsjoin-fullnode.sh TO THE FOLDER WHERE"
-  echo -e "BITCOIN WILL GO AND RUN AGAIN.\n\n"
-  read -p "PRESS ENTER TO EXIT. "
+
+
+# Check for root.
+if [[ $(id -u) = "0" ]]; then
+  echo "
+YOU SHOULD NOT RUN THIS SCRIPT AS ROOT!
+YOU WILL BE PROMPTED FOR THE ADMIN PASS WHEN NEEDED.
+"
+  read -p "PRESS ENTER TO EXIT SCRIPT, AND RUN AGAIN AS amnesia. "
   exit 0
 fi
+
+
+# Make sure user has chosen the correct script.
+echo "
+          THIS SCRIPT WILL INSTALL JOINMARKET, DEPENDENCIES,
+              AND BITCOIN CORE. YOU MUST HAVE AT LEAST
+                 50GB OF FREE SPACE ON YOUR DEVICE.
+
+             ADMIN PASS WILL BE REQUIRED MULTIPLE TIMES.
+"
+read -p "PRESS ENTER TO CONTINUE. "
 clear
-echo -e "\n\nYOU NOW HAVE THE OPTION OF DOWNLOADING THE STANDARD BITCOIN CORE"
-echo -e " CLIENT, OR THE BITCOIN XT CLIENT WITH SUPPORT FOR BIGGER BLOCKS.\n"
-read -p "WOULD YOU LIKE TO DOWNLOAD (C)ORE OR (X)T (c/x)? " cx
-if [[ "$cx" = "c" || "$cx" = "C" ]]; then
-  clear
-  echo -e "\n\nPRESS ENTER TO GET BITCOIN CORE AND CHECKSUMS FROM:\n"
-  echo "https://bitcoin.org/bin/bitcoin-core-0.11.1/bitcoin-0.11.1-linux32.tar.gz"
-  echo "https://bitcoin.org/bin/bitcoin-core-0.11.1/SHA256SUMS.asc "; read
-  curl -x socks5://127.0.0.1:9050 -# -L -O https://bitcoin.org/bin/bitcoin-core-0.11.1/bitcoin-0.11.1-linux32.tar.gz -O https://bitcoin.org/bin/bitcoin-core-0.11.1/SHA256SUMS.asc
-  clear
-  echo -e "\n\nIMPORTING KEY: 0x90C8019E36C2E964 TO CHECK SIG."
-  gpg --recv-keys 01EA5486DE18A882D4C2684590C8019E36C2E964
-  gpg --verify SHA256SUMS.asc
-  echo -e "\n\nPLEASE REVIEW THE SIG TO MAKE SURE IT IS GOOD.\n"
-  read -p "GOOD SIG? (y/n) " x
-  while [[ "$x" = "n" || "$x" = "N" ]]; do
+
+
+# Check for persistence.
+if  [[ $(pwd | grep -c Persistent) = "0" && -e /home/amnesia/Persistent ]]; then
+  echo "
+  IT SEEMS YOU HAVE PERSISTENCE ENABLED, BUT YOU ARE IN THE FOLDER:
+  "$PWD"
+  IF YOU MOVE THE tailsjoin/ FOLDER TO /home/amnesia/Persistent/
+  YOUR INSTALL WILL SURVIVE REBOOTS, OTHERWISE IT WILL NOT.
+  "
+  read -p "QUIT THE SCRIPT NOW TO MOVE? (y/n) " q
+  if  [[ "$q" = "y" || "$q" = "Y" ]]; then
+    exit 0
+  else
     clear
-    echo -e "\n\n"; read -p "PRESS ENTER TO DELETE FILES AND GET AGAIN."
-    srm -drv SHA256SUMS.asc
-    curl -x socks5://127.0.0.1:9050 -# -L -O https://bitcoin.org/bin/bitcoin-core-0.11.1/SHA256SUMS.asc
-    gpg --verify SHA256SUMS.asc
-    echo -e "\n\nPLEASE REVIEW THE SIG TO MAKE SURE IT IS GOOD.\n"
-    read -p "GOOD SIG? (y/n) " x
-  done
+  fi
+fi
+
+
+# Download/verify Bitcoin Core.
+echo "
+GETTING BITCOIN CORE, CHECKSUM, AND SIGNING KEYS...
+"
+curl -x socks5://127.0.0.1:9050 -# -L -O https://bitcoin.org/bin/bitcoin-core-0.11.1/bitcoin-0.11.1-linux32.tar.gz -O https://bitcoin.org/bin/bitcoin-core-0.11.1/SHA256SUMS.asc
+gpg --recv-keys 01EA5486DE18A882D4C2684590C8019E36C2E964
+echo "01EA5486DE18A882D4C2684590C8019E36C2E964:6" | gpg --import-ownertrust -
+clear
+echo "
+VERIFYING SIGNATURE...
+"
+gpg --verify SHA256SUMS.asc
+echo "
+PLEASE REVIEW THE TEXT ABOVE.
+IT WILL EITHER SAY GOOD SIG OR BAD SIG.
+"
+read -p "IS IT A GOOD SIG? (y/n) " x
+while [[ "$x" = "n" || "$x" = "N" ]]; do
   clear
-  echo -e "\n\nCHECKING THE SHA256 SUM OF DOWNLOADED BITCOIN CLIENT."
-  sha=$(grep linux32.tar.gz SHA256SUMS.asc | cut -b -64)
+  echo "
+SECURELY DELETING FILES AND DOWNLOADING AGAIN...
+"
+  srm -drv SHA256SUMS.asc
+  curl -x socks5://127.0.0.1:9050 -# -L -O https://bitcoin.org/bin/bitcoin-core-0.11.1/SHA256SUMS.asc
+  clear
+  echo "
+VERIFYING CHECKSUMS...
+"
+  gpg --verify SHA256SUMS.asc
+  echo "
+PLEASE REVIEW THE TEXT ABOVE.
+IT WILL EITHER SAY GOOD SIG OR BAD SIG.
+"
+  read -p "IS IT A GOOD SIG? (y/n) " x
+done
+clear
+echo "
+VERIFYING CHECKSUM OF BITCOIN CORE...
+"
+sha=$(grep linux32.tar.gz SHA256SUMS.asc | cut -b -64)
+echo ""; echo ""$sha"  bitcoin-0.11.1-linux32.tar.gz" | sha256sum -c
+echo""; read -p 'DID THAT SHOW: "bitcoin-0.11.1-linux32.tar.gz: OK" ? (y/n) ' x
+while [[ "$x" = "n" || "$x" = "N" ]]; do
+  clear
+  echo "
+SECURELY DELETING FILES AND DOWNLOADING AGAIN...
+"
+  srm -drv bitcoin*.tar.gz
+  curl -x socks5://127.0.0.1:9050 -# -L -O https://bitcoin.org/bin/bitcoin-core-0.11.1/bitcoin-0.11.1-linux32.tar.gz
   echo ""; echo ""$sha"  bitcoin-0.11.1-linux32.tar.gz" | sha256sum -c
   echo""; read -p 'DID THAT SHOW: "bitcoin-0.11.1-linux32.tar.gz: OK" ? (y/n) ' x
-  while [[ "$x" = "n" || "$x" = "N" ]]; do
-    clear
-    echo -e "\n\n"; read -p "PRESS ENTER TO DELETE FILES AND GET AGAIN."
-    srm -drv bitcoin*.tar.gz
-    curl -x socks5://127.0.0.1:9050 -# -L -O https://bitcoin.org/bin/bitcoin-core-0.11.1/bitcoin-0.11.1-linux32.tar.gz
-    echo ""; read -p "PRESS ENTER TO CHECK THE SHA256 SUM OF DOWNLOADED BITCOIN CLIENT."
-    sha=$(grep linux32.tar.gz SHA256SUMS.asc | cut -b -64)
-    echo ""; echo ""$sha"  bitcoin-0.11.1-linux32.tar.gz" | sha256sum -c
-    echo""; read -p 'DID THAT SHOW: "bitcoin-0.11.1-linux32.tar.gz: OK" ? (y/n) ' x
-  done
-  clear
-  echo -e "\n\nPRESS ENTER TO EXTRACT BITCOIN AND DELETE USELESS FILES."; read
-  tar -xvf bitcoin*.tar.gz
-  rm -rf bitcoin*.tar.gz SHA256SUMS.asc
-else
-  clear
-  echo -e "\n\nPRESS ENTER TO GET BITCOIN XT FROM:\n"
-  echo "https://github.com/bitcoinxt/bitcoinxt/releases/download/v0.11C/bitcoin-xt-0.11.0-C-linux32.tar.gz "; read
-  curl -x socks5://127.0.0.1:9050 -# -L -O https://github.com/bitcoinxt/bitcoinxt/releases/download/v0.11C/bitcoin-xt-0.11.0-C-linux32.tar.gz
-  clear
-  echo -e "\n\nCHECKING THE SHA256 SUM OF DOWNLOADED BITCOIN CLIENT."
-  sha=5dd32a6f30d7fd05b271908a53a2c9db7baa629b9299e40fd17a9009d59c9348
-  echo ""; echo ""$sha"  bitcoin-xt-0.11.0-C-linux32.tar.gz" | sha256sum -c
-  echo""; read -p 'DID THAT SHOW: "bitcoin-xt-0.11.0-C-linux32.tar.gz: OK" ? (y/n) ' x
-  while [[ "$x" = "n" || "$x" = "N" ]]; do
-    clear
-    echo -e "\n\n"; read -p "PRESS ENTER TO DELETE FILES AND GET AGAIN. "
-    srm -drv bitcoin-xt-0.11.0-C-linux32.tar.gz
-    curl -x socks5://127.0.0.1:9050 -# -L -O https://github.com/bitcoinxt/bitcoinxt/releases/download/v0.11C/bitcoin-xt-0.11.0-C-linux32.tar.gz
-    echo ""; read -p "PRESS ENTER TO CHECK THE SHA256 SUM OF DOWNLOADED BITCOIN CLIENT."
-    sha=5dd32a6f30d7fd05b271908a53a2c9db7baa629b9299e40fd17a9009d59c9348
-    echo ""; echo ""$sha"  bitcoin-xt-0.11.0-C-linux32.tar.gz" | sha256sum -c
-    echo""; read -p 'DID THAT SHOW: "bitcoin-xt-0.11.0-C-linux32.tar.gz: OK" ? (y/n) ' x
-  done
-  clear
-  echo -e "\n\nPRESS ENTER TO EXTRACT BITCOIN AND DELETE USELESS FILES."; read
-  tar -xvf bitcoin*.tar.gz
-  rm -rf bitcoin*.tar.gz
-fi
+done
+echo "
+EXTRACTING BITCOIN CORE, DELETING USELESS FILES...
+"
+tar xvf bitcoin*.tar.gz
+rm -rf bitcoin*.tar.gz SHA256SUMS.asc
+mv bitcoin-0.11.1/ ..
 clear
-rpcu=$(pwgen -ncsB 35 1)
-rpcp=$(pwgen -ncsB 75 1)
-echo -e "\n\nPRESS ENTER TO PUT THESE SETTINGS IN YOUR BITCOIN.CONF:\n"
-bitconf=$(echo -e "daemon=1\nrpcuser="$rpcu"\nrpcpassword="$rpcp"\nproxy=127.0.0.1:9050\nlisten=0\nproxyrandomize=1\nserver=1\n\n# JoinMarket Settings\nwalletnotify=curl -sI --connect-timeout 1 http://127.0.0.1:62602/walletnotify?%s\nalertnotify=curl -sI --connect-timeout 1 http://127.0.0.1:62062/alertnotify?%s\n# User to input blockchain path\ndatadir=")
-echo "$bitconf"
-read
-if [ -e "bitcoin-0.11.1/bin/bitcoin.conf" ]; then
+
+
+# Check for bitcoin.conf
+if [ -e "../bitcoin-0.11.1/bin/bitcoin.conf" ]; then
   clear
   echo ""; read -p 'FILE "bitcoin.conf" EXISTS. OVERWRITE? (y/n) ' ow
   if  [[ "$ow" = "n" || "$ow" = "N" ]]; then
@@ -94,64 +113,123 @@ if [ -e "bitcoin-0.11.1/bin/bitcoin.conf" ]; then
     exit 0
   fi
 fi
-echo "$bitconf" > bitcoin-0.11.1/bin/bitcoin.conf
-clear
-echo -e "\nYOU WILL NEED TO ENTER YOUR DATA DIRECTORY IN THE CONFIG."
-echo -e "CONFIG FILE IS LOCATED AT: bitcoin-0.11.1/bin/bitcoin.conf"
+
+
+# Create a bitcoin.conf.
+rpcu=$(pwgen -ncsB 35 1)
+rpcp=$(pwgen -ncsB 75 1)
+echo "rpcuser="$rpcu"
+rpcpassword="$rpcp"
+daemon=1
+proxyrandomize=1
+proxy=127.0.0.1:9050
+listen=0
+server=1
+
+# For JoinMarket
+walletnotify=curl -sI --connect-timeout 1 http://127.0.0.1:62602/walletnotify?%s
+alertnotify=curl -sI --connect-timeout 1 http://127.0.0.1:62062/alertnotify?%s
+
+# User must uncomment and input path to blockchain files
+#datadir=" > ../bitcoin-0.11.1/bin/bitcoin.conf
+
+
+# Fix iptables for RPC calls from amnesia.
 # This code was lifted from Axis-Mundi README.TAILS (https://github.com/six-pack/Axis-Mundi/blob/master/README.TAILS)
-echo -e "\n\nENTER PASSWORD TO ALLOW CALLS TO LOCALHOST BY ADJUSTING IPTABLES AS FOLLOWS:\n"
-echo -e "sudo iptables -I OUTPUT 2 -p tcp -s 127.0.0.1 -d 127.0.0.1 -m owner --uid-owner amnesia -j ACCEPT\n"
+echo "
+ENTER PASSWORD TO ADJUST IPTABLES SO USER amnesia CAN TALK TO BITCOIN.
+"
 sudo iptables -I OUTPUT 2 -p tcp -s 127.0.0.1 -d 127.0.0.1 -m owner --uid-owner amnesia -j ACCEPT
 clear
-echo -e "\n\nNOW WE WILL INSTALL JOINMARKET AND ITS DEPENDENCIES."
-echo -e "\nENTER PASSWORD AT PROMPT TO UPDATE SOURCES.\n"
+
+
+# Update apt-get sources.
+echo "
+ENTER PASSWORD TO UPDATE SOURCES.
+"
 sudo apt-get update
 clear
-echo -e "\n\nENTER PASSWORD AT PROMPT TO INSTALL THE FOLLOWING DEPENDENCIES:\n"
-echo -e "gcc, libc6-dev, make\n"
+
+
+# Install dependencies for building libsodium.
+echo "
+ENTER PASSWORD TO INSTALL: 'gcc', 'libc6-dev', and 'make'
+(NEEDED TO BUILD LIBSODIUM CRYPTO LIBRARY)
+"
 sudo apt-get install -y gcc libc6-dev make
 clear
-echo -e "\n\nPRESS ENTER TO CLONE INTO JOINMARKET VIA:\n"
-read -p "https://github.com/chris-belcher/joinmarket "
+
+
+# Clone JoinMarket.
 git clone https://github.com/chris-belcher/joinmarket ../joinmarket
-# Fetch libsodium and sig, import key, and verify.
 clear
-echo -e "\n\nPRESS ENTER TO GET SIGNING KEYS AND VERIFY LIBSODIUM SOURCE FROM:\n"
-read -p "http://download.libsodium.org/libsodium/releases/libsodium-1.0.4.tar.gz "
+
+
+# Get libsodium, sig, and import key.
+echo "
+DOWNLOADING LIBSODIUM SOURCE AND SIGNING KEY...
+"
 gpg --recv-keys 54A2B8892CC3D6A597B92B6C210627AABA709FE1
 echo "54A2B8892CC3D6A597B92B6C210627AABA709FE1:6" | gpg --import-ownertrust -
 curl -x socks5://127.0.0.1:9050 -# -L -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.4.tar.gz -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.4.tar.gz.sig
+clear
+
+
+# Verify download.
+echo "
+VERIFYING THE DOWNLOAD...
+"
 gpg --verify libsodium-1.0.4.tar.gz.sig libsodium-1.0.4.tar.gz
-echo -e "\n\nPLEASE REVIEW SIGNATURE.\n"
-read -p "GOOD SIG? (y/n) " x
+echo "
+PLEASE REVIEW THE TEXT ABOVE.
+IT WILL EITHER SAY GOOD SIG OR BAD SIG.
+"
+read -p "IS IT A GOOD SIG? (y/n) " x
 while [[ "$x" = "n" || "$x" = "N" ]]; do
   clear
-  echo -e "\n\n"
-  read -p "PRESS ENTER TO DELETE FILES AND GET AGAIN. "
+  echo "
+SECURELY DELETING FILES AND DOWNLOADING AGAIN...
+"
   srm -drv libsodium*
   curl -x socks5://127.0.0.1:9050 -# -L -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.4.tar.gz -O http://download.libsodium.org/libsodium/releases/libsodium-1.0.4.tar.gz.sig
   gpg --verify libsodium-1.0.4.tar.gz.sig libsodium-1.0.4.tar.gz
-  echo -e "\n\nPLEASE REVIEW THE SIG TO MAKE SURE IT IS GOOD.\n"
-  read -p "GOOD SIG? (y/n) " x
+  echo "
+PLEASE REVIEW THE TEXT ABOVE.
+IT WILL EITHER SAY GOOD SIG OR BAD SIG.
+"
+  read -p "IS IT A GOOD SIG? (y/n) " x
 done
-# Build libsodium, install, and delete tar files
+clear
+
+
+# Build and install libsodium.
 tar xf libsodium*.tar.gz
 rm -rf libsodium*.tar.gz*
-if [[ $(echo "$PWD" | grep -c Persistent) = "1" ]]; then
-  clear
-  echo -e "\n\nPRESS ENTER TO BUILD AND INSTALL LIBSODIUM IN A WAY THAT WILL SURVIVE REBOOTS.\n"
-  read
+
+if [[ $(pwd | grep -c Persistent) = "1" ]]; then
+  echo "
+BUILDING AND INSTALLING LIBSODIUM TO SURVIVE REBOOTS...
+"
   mkdir ../joinmarket/libsodium
-  ( cd libsodium-1.0.4/ && ./configure --prefix=$(echo "$PWD") && make && make install )
+  ( cd libsodium-1.0.4/ && ./configure --prefix=$(pwd) && make && make install )
   mv libsodium-1.0.4/lib/libsodium.* ../joinmarket/libsodium/
-  sed -i "s|\/usr\/local\/lib|$(echo "$PWD" | sed 's|tailsjoin|joinmarket\/libsodium|')|" ../joinmarket/lib/libnacl/__init__.py
+  sed -i "s|\/usr\/local\/lib|$(pwd | sed 's|tailsjoin|joinmarket\/libsodium|')|" ../joinmarket/lib/libnacl/__init__.py
+  ( cd ../joinmarket && git commit -a -m "Tailsjoin survive reboots" )
 else
+  echo "
+BUILDING LIBSODIUM...
+"
   ( cd libsodium-1.0.4/ && ./configure && make )
-  clear
-  echo -e "\n\nLIBSODIUM SUCCESSFULLY BULIT. ENTER PASSWORD TO INSTALL.\n"
+  echo "
+LIBSODIUM SUCCESSFULLY BULIT. ENTER PASSWORD TO INSTALL.
+"
   ( cd libsodium-1.0.4/ && sudo make install )
 fi
-rm -rf libsodium-1.0.4/
+rm -rf libsodium*
+clear
+
+
+# Set JoinMarket config for tor and bitcoin-rpc.
 echo "[BLOCKCHAIN]
 blockchain_source = bitcoin-rpc
 # blockchain_source options: blockr, bitcoin-rpc, json-rpc, regtest
@@ -159,8 +237,8 @@ blockchain_source = bitcoin-rpc
 network = mainnet
 rpc_host = 127.0.0.1
 rpc_port = 8332
-rpc_user = $rpcu
-rpc_password = $rpcp
+rpc_user = "$rpcu"
+rpc_password = "$rpcp"
 
 [MESSAGING]
 #host = irc.cyberguerrilla.org
@@ -177,19 +255,33 @@ host = 6dvj6v5imhny3anf.onion
 maker_timeout_sec = 60
 
 [POLICY]
-# merge_algorithm options: greedy, default, gradual
+# merge_algorithm options: greediest, greedy, default, gradual
 merge_algorithm = default" > ../joinmarket/joinmarket.cfg
-clear
-echo -e "\n\nJOINMARKET CLONED, AND CONFIG SET TO USE TOR AND BITCOIN RPC!\n"
-read -p "PRESS ENTER FOR SOME FINAL NOTES. "
-clear
-echo -e "\n\nPLEASE GO HERE TO GET DETAILED INFO ON HOW TO OPERATE FROM THE CREATOR:"
-echo "https://github.com/chris-belcher/joinmarket/wiki"
-echo -e "\nYOU CAN RUN BITCOIN BY ENTERING THE FOLDER:"
-echo " $PWD/bitcoin-0.11.1/bin/"
-echo "AND USE THIS COMMAND:"
-echo "./bitcoind -conf=$PWD/bitcoin-0.11.1/bin/bitcoin.conf"
-echo -e "\nYOU MUST NOW ENTER YOUR DATA DIR IN THE BITCOIN CONFIG FILE."
-echo -e "EXAMPLE ENTRY:\ndatadir=/media/mounted_device/.bitcoin/\n\n"
-read -p "PRESS ENTER TO LEAVE SCRIPT. "
+
+
+# Final notes.
+echo "
+          JOINMARKET SUCCESSFULLY INSTALLED AND CONFIGURED!
+
+          IF YOU HAVE PERSISTENCE ENABLED, AND YOU RAN THIS
+       SCRIPT FROM WITHIN THE FOLDER: /home/amnesia/Persistent,
+               THEN YOUR INSTALL WILL SURVIVE REBOOTS.
+
+YOUR JOINMARKET FOLDER LOCATION:
+$(pwd | sed 's|tailsjoin|joinmarket|')
+
+YOUR BITCOIN FOLDER LOCATION:
+$(pwd | sed 's|tailsjoin|bitcoin-0.11.1|')
+
+YOUR BITCOIN.CONF LOCATION:
+$(pwd | sed 's|tailsjoin|bitcoin-0.11.1\/bin\/bitcoin.conf|')
+
+    YOU MUST NOW ENTER YOUR DATA DIRECTORY (BLOCKCHAIN FILES)
+    INTO YOUR BITCOIN.CONF OR BITCOIN WILL START FROM SCRATCH.
+    TO RUN BITCOIN, ENTER THE FOLDER:
+    $(pwd | sed 's|tailsjoin|bitcoin-0.11.1\/bin|')
+    AND DO:
+    ./bitcoind conf=$(pwd | sed 's|tailsjoin|bitcoin-0.11.1\/bin\/bitcoin.conf|')
+"
+read -p "PRESS ENTER TO EXIT SCRIPT. "
 exit 0
